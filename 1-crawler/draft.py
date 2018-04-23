3a392e78df300e827b2c3bf6e15fc2ddc6c49b52
Qing Peng <pengqing2012@gmail.com>
2018-04-24 01:12:29 +0200

    Update

--- 1-crawler/draft.py
+++ 1-crawler/draft.py
@@ -1,6 +1,1 @@
-from scrapy.selector import Selector
-body = open('./5-scrapy/movie/imdbtop250.txt','r').read()
-#print(body)
-a = Selector(text=body).xpath('//tbody[@class="lister-list"]/tr/td[@class="posterColumn"]/span[@name="ir"]/@data-value').extract()
-#b = Selector(text=a).xpath('./td[@class="titleColumn"]/a').extract()
-print(a)
+draft
